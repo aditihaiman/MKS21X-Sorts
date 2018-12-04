0f6c7a8894ff22cf8320870121e5367ea6b0b1a1
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-04 11:01:29 -0500

    renamed file

---
+++ selectionSort.java
@@ -0,0 +1,27 @@
+public class selectionSort {
+  public static void main(String[] args) {
+    int[] ary = {1, 6, 3, 2, 7, 4};
+    System.out.println("[1, 6, 3, 2, 7, 4]");
+    insertionSort(ary);
+    for (int x = 0; x < ary.length; x++) {
+      System.out.println(ary[x]);
+    }
+  }
+  public static void insertionSort(int[] ary) {
+    int tempIdx = 0;
+    int x = 0;
+    for (int i = 0; i < ary.length; i++) {
+      int min = ary[i];
+      for (x = i; x < ary.length; x++) {
+        if (ary[x] < min) {
+          min = ary[x];
+          tempIdx = x;
+        }
+      }
+      ary[tempIdx] = ary[i];
+      ary[i] = min;
+    }
+  } 
+
+
+}
