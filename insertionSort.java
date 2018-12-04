8855620ca11e48e4b01b9243a3e452d9d6879778
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-04 10:53:25 -0500

    fixed problems

--- insertionSort.java
+++ insertionSort.java
@@ -1,7 +1,7 @@
 public class insertionSort {
   public static void main(String[] args) {
-    int[] ary = {9, 6, 3, 2, 7, 4};
-    System.out.println("[9, 6, 3, 2, 7, 4]");
+    int[] ary = {5, 6, 3, 2, 7, 4};
+    System.out.println("[5, 6, 3, 2, 7, 4]");
     insertionSort(ary);
     for (int x = 0; x < ary.length; x++) {
       System.out.println(ary[x]);
@@ -9,18 +9,17 @@ public class insertionSort {
   }
   public static void insertionSort(int[] ary) {
     int tempIdx = 0;
-    int min = ary[0];
     int x = 0;
     for (int i = 0; i < ary.length; i++) {
+      int min = ary[i];
       for (x = i; x < ary.length; x++) {
         if (ary[x] < min) {
           min = ary[x];
           tempIdx = x;
         }
       }
-      int temp = ary[i];
+      ary[tempIdx] = ary[i];
       ary[i] = min;
-      ary[tempIdx] = temp;
     }
   }
 
