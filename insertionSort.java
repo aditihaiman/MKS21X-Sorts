bc31255995584dbbef8167a6b98b27276c17e4c9
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-04 10:48:34 -0500

    trying to fix index out of bounds

--- insertionSort.java
+++ insertionSort.java
@@ -1,19 +1,26 @@
 public class insertionSort {
   public static void main(String[] args) {
-    int[] ary = {1, 6, 3, 2, 7, 4};
-    System.out.println("[1, 6, 3, 2, 7, 4]");
+    int[] ary = {9, 6, 3, 2, 7, 4};
+    System.out.println("[9, 6, 3, 2, 7, 4]");
     insertionSort(ary);
-    System.out.println(ary);
+    for (int x = 0; x < ary.length; x++) {
+      System.out.println(ary[x]);
+    }
   }
   public static void insertionSort(int[] ary) {
+    int tempIdx = 0;
     int min = ary[0];
-    int x;
+    int x = 0;
     for (int i = 0; i < ary.length; i++) {
       for (x = i; x < ary.length; x++) {
-        if (ary[x] < min) min = ary[x];
+        if (ary[x] < min) {
+          min = ary[x];
+          tempIdx = x;
+        }
       }
-      ary[x] = ary[i];
+      int temp = ary[i];
       ary[i] = min;
+      ary[tempIdx] = temp;
     }
   }
 
