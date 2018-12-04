092108eb0d95754f928fc8d7aef3a805e5641dc7
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-04 10:37:33 -0500

    testing

--- insertionSort.java
+++ insertionSort.java
@@ -1,4 +1,10 @@
 public class insertionSort {
+  public static void main(String[] args) {
+    int[] ary = {1, 6, 3, 2, 7, 4};
+    System.out.println("[1, 6, 3, 2, 7, 4]");
+    insertionSort(ary);
+    System.out.println(ary);
+  }
   public static void insertionSort(int[] ary) {
     int min = ary[0];
     int x;
