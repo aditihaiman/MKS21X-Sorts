6b7a084e5d3e34f1571baa65052b1d0581b95db5
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-05 09:42:53 -0500

    tested method again

--- selectionSort.java
+++ selectionSort.java
@@ -1,12 +1,22 @@
+import java.util.Arrays;
 public class selectionSort {
+
   public static void main(String[] args) {
     int[] ary = {1, 6, 3, 2, 7, 4};
     System.out.println("[1, 6, 3, 2, 7, 4]");
     selectionSort(ary);
-    for (int x = 0; x < ary.length; x++) {
-      System.out.println(ary[x]);
-    }
+    System.out.println(Arrays.toString(ary)); //[1, 2, 3, 4, 6, 7]
+    System.out.println("–––––––––––––––––––––––––––––––");
+    int[] ary2 = {5, 1, 12, -5, 16, 2, 12, 14};
+    System.out.println("[5, 1, 12, -5, 16, 2, 12, 14]");
+    selectionSort(ary2);
+
+
+
   }
+
+
+
   public static void selectionSort(int[] ary) {
     int minIdx;
     int x = 0;
@@ -20,6 +30,7 @@ public class selectionSort {
       int temp = ary[i];
       ary[i] = ary[minIdx];
       ary[minIdx] = temp;
+      System.out.println(Arrays.toString(ary));
     }
   }
 
