204877bd178197a7a826f72c600ac261eef672f1
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-07 13:15:06 -0500

    tested runtime for both sorts

--- Driver.java
+++ Driver.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.lang.Math;
 public class Driver {
   public static void main(String[] args) {
     int[] data = {3, 5, 7, 5, 4, 2, 1, 9, 0};
@@ -9,6 +10,15 @@ public class Driver {
     int[] data2 = {};
     Sorts.bubbleSort(data2);
     System.out.println(Arrays.toString(data2));
+
+
+    int[] data3 = new int[100000];
+    for(int x = 0; x < 100000; x++) {
+      data3[x] = (int)(Math.random() * 10);
+    }
+
+    Sorts.selectionSort(data3);
+    //System.out.println(Arrays.toString(data3));
 
   }
 }
--- Sorts.java
+++ Sorts.java
@@ -33,10 +33,8 @@ public class Sorts {
       }
       if (swaps==0) swap = false;
     }
-
-
-
   }
+
 
 
 }
