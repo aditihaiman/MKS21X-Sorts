9d468890be872a73b87102acf1dfc752b9481fcd
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-12-07 09:58:59 -0500

    testing bubble sort

---
+++ Driver.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Driver {
+  public static void main(String[] args) {
+    int[] data = {3, 5, 7, 5, 4, 2, 1, 9, 0};
+    System.out.println(Arrays.toString(data));
+    Sorts.bubbleSort(data);
+    System.out.println(Arrays.toString(data));
+
+
+  }
+}
